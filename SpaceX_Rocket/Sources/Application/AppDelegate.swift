2f6ab11550cf003ca91b9fa5361d45811e39c707
elenoack <elenanoack@mail.ru>
2022-06-15 23:18:36 +0300

    folder_structure

---
+++ SpaceX_Rocket/Sources/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpaceX_Rocket
+//
+//  Created by mac on 15.06.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
